80d02e39db27806e1e02fac8638c7ae79d2ee384
christoskv <christoskavallaris@gmail.com>
2018-06-20 23:45:00 +0300

    Added level Loader in Testing Scene
    Script that creates a progress bar when loading a Scene

---
+++ Projects/Testing Scene/Assets/LevelLoader/Scripts/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour {
+
+    public GameObject loadingScene;
+    public Slider slider;
+    public TMPro.TextMeshProUGUI progressText;
+
+    private void Start()
+    {
+        loadingScene.SetActive(false);
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        StartCoroutine( LoadAsynchronously(sceneName) );
+    }
+
+    IEnumerator LoadAsynchronously(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        loadingScene.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+
+            yield return null;
+        }
+    }
+}
